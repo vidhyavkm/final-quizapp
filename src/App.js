56e28aa7a167cb699183cb644a8e7e7097855984
vidhya <vidhyavkm@gmail.com>
2022-07-14 17:18:23 +0530

    make it better

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import QuestList from "./elements/QuestList";
+import {v4 as uuidv4} from "uuid";
+import './App.css';
+
+const App = () => {
+  const [currentQuest, setCurrentQuest] = useState(0);
+  const [score, setScore] = useState(0);
+  const [clicked,setClicked] = useState(false);
+  const[showScore, setShowScore] = useState(false); 
+
+  const handleCorrectAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setScore( score + 1);
+    }
+    setClicked(true);
+  };
+  const handleNextQuest = ()=> {
+    setClicked (false);
+    if (currentQuest < QuestList.length - 1){
+      setCurrentQuest(currentQuest + 1);
+    } else {
+      setShowScore(true);
+    }
+  }
+
+  return (
+  <div className = "app-wrapper"> 
+  {showScore ? (
+    <div>
+      <div className = "completed"> Completed! </div>
+      <div className = "score-section">
+        Your Score : {score} /{QuestList.length} 
+        </div>
+    </div>
+  ) : (
+
+  <div> 
+    <div className ="quest-section-wrapper">
+<div className ="quest-count"> 
+Question {currentQuest + 1} of {QuestList.length}
+</div>
+<div className ="question">
+  {QuestList[currentQuest].quest}
+</div>
+    </div>
+    <div className = "answer-section-wrapper">
+      {QuestList [currentQuest].answersList.map((answerOption) =>(
+<li className = "answer-list" key = {uuidv4()}>
+<button 
+disabled = {clicked}
+className = {`answer-button ${clicked && answerOption.isCorrect ? "correct" : ""}`} 
+onClick = {() =>handleCorrectAnswer(answerOption.isCorrect)}
+> 
+  {answerOption.answer}
+</button>
+</li>
+      ))}
+    </div>
+    <div>
+      <button className ="next-button" onClick = {handleNextQuest}
+      disabled = {!clicked}
+      > 
+        Next
+        </button>
+    </div>
+    </div>
+  )}
+  </div>
+  );
+};
+
+export default App;
---
+++ src/elements/QuestList.js
@@ -0,0 +1,48 @@
+const QuestList = [
+    {
+        quest : "What is the full form of WWW?",
+        answersList : [
+            { answer : "World Width Web" , isCorrect:false   },
+            { answer : "World War Won" , isCorrect:false   },
+            { answer : "World Wide Web" , isCorrect:true   },
+            { answer : "World Web Wide" , isCorrect:false   },
+        ],
+    },
+    {
+        quest : "What is the capital of India?",
+        answersList : [
+            { answer : "Delhi" , isCorrect:true   },
+            { answer : "Kolkata" , isCorrect:false   },
+            { answer : "Mumbai" , isCorrect:false   },
+            { answer : "Chennai" , isCorrect:false   },
+        ],
+    },
+    {
+        quest : "What is the highest peak in the world?",
+        answersList : [
+            { answer : "Mount K2" , isCorrect:false   },
+            { answer : "Alps mount" , isCorrect:false   },
+            { answer : "Machu Pichu" , isCorrect:false   },
+            { answer : "Mount Everest" , isCorrect:true  },
+        ],      
+    },
+    {
+        quest : "Who is the highest earner in the world?",
+        answersList : [
+            { answer : "Jeff Bezos" , isCorrect:false   },
+            { answer : "Bill Gates" , isCorrect:false   },
+            { answer : "Elon Musk" , isCorrect:true   },
+            { answer : "Mukesh Ambani" , isCorrect:false   },
+        ],      
+    },
+    {
+        quest : "Who maintains React?",
+        answersList : [
+            { answer : "Google" , isCorrect:false   },
+            { answer : "Facebook" , isCorrect:true   },
+            { answer : "Amazon" , isCorrect:false   },
+            { answer : "Apple" , isCorrect:false   },
+        ],      
+    },
+];
+export default QuestList;
